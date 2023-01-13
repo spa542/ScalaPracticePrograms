0cb4aefd12199085b616628dc93530728cbade33
Ryan Rosiak <rosi@ryans-mbp.mynetworksettings.com>
2023-01-12 21:56:50 -0500

    Add changes to scala program

--- BigDataHadoopSparkWithScala/SparkSubmitScala/src/main/scala/HelloSpark.scala
+++ BigDataHadoopSparkWithScala/SparkSubmitScala/src/main/scala/HelloSpark.scala
@@ -9,11 +9,9 @@ object HelloSpark {
     val ss: SparkSession = SparkSession.builder().master("yarn").appName("hello_spark_test").getOrCreate()
     // Create a dataframe
     val sample_df: RDD[Int] = ss.sparkContext.parallelize(List(1,2,3,4,5,6,7,8,9))
-    println(sample_df.collect().mkString)
     val modified_df: RDD[Int] = sample_df.map(_ + 10)
-    println(modified_df.collect().mkString)
-    sample_df.saveAsTextFile("./sample_df.txt")
-    modified_df.saveAsTextFile("./modified_df.txt")
+    sample_df.saveAsTextFile("file:///home/ubuntu/sample_df.txt")
+    modified_df.saveAsTextFile("file:///home/ubuntu/modified_df.txt")
   }
 
 }
