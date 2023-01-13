42349a3950d4901478d7459917eb48148e67f7b1
Ryan Rosiak <rosi@ryans-mbp.mynetworksettings.com>
2023-01-12 21:59:29 -0500

    Add another small change to write to hdfs

--- BigDataHadoopSparkWithScala/SparkSubmitScala/src/main/scala/HelloSpark.scala
+++ BigDataHadoopSparkWithScala/SparkSubmitScala/src/main/scala/HelloSpark.scala
@@ -10,8 +10,8 @@ object HelloSpark {
     // Create a dataframe
     val sample_df: RDD[Int] = ss.sparkContext.parallelize(List(1,2,3,4,5,6,7,8,9))
     val modified_df: RDD[Int] = sample_df.map(_ + 10)
-    sample_df.saveAsTextFile("file:///home/ubuntu/sample_df.txt")
-    modified_df.saveAsTextFile("file:///home/ubuntu/modified_df.txt")
+    sample_df.saveAsTextFile("hdfs:///spark/sample_df.txt")
+    modified_df.saveAsTextFile("hdfs:///spark/modified_df.txt")
   }
 
 }
