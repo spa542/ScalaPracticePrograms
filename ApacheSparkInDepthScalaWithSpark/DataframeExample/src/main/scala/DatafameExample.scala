a08f92cab583cad62732e4cdac4a3bfabedebd10
Ryan Rosiak <rosi@ryans-mbp.mynetworksettings.com>
2023-02-05 13:11:55 -0500

    Add finished dataframe examples with jar and run script

--- ApacheSparkInDepthScalaWithSpark/DataframeExample/src/main/scala/DatafameExample.scala
+++ ApacheSparkInDepthScalaWithSpark/DataframeExample/src/main/scala/DatafameExample.scala
@@ -14,8 +14,13 @@ object DatafameExample {
     // How to use hive sql
     import ss.sql
 
-    // TODO MAKE SURE TO MOVE DATA TO CORRECT LOCATION ON NODE
+    // TODO MAKE SURE TO DOWNLOAD AND MOVE DATA TO CORRECT LOCATION ON NODE
     val storeDF = ss.read.format("json").load("/dataframeExampleData/store_locations.json")
+    // TODO MAKE SURE TO DOWNLOAD AND MOVE DATA TO CORRECT LOCATION ON NODE
+    val carDF = ss.read.format("csv").option("header", "true").option("inferSchema", "true")
+      .load("/dataframeExampleData/CarPrice_Assignment.csv")
+
+    // Starting with storeDF from here on out
     println("Store Data")
     storeDF.show(5)
 
@@ -102,6 +107,92 @@ object DatafameExample {
     // repartition and coalesce (same as RDD) (.rdd converts to an rdd)
     println(storeDF.rdd.getNumPartitions)
 
+    // Starting with carDF from here on out
+    carDF.createOrReplaceTempView("carDFView")
+    ss.sql("""select * from carDFView""").show(1)
+
+    carDF.select("*").show(1)
+
+    // List the car names where price > 30000 (can do either way)
+    carDF.where("price > 30000").select("CarName", "price").show(5)
+    ss.sql("""select CarName, price form carDFView where price > 30000""").show(5)
+
+    // Find the combined average of highway mpg and city mpg for all cars
+    val avgMPG = (col("citympg") + col("highwaympg")) / 2
+    // One Way (if using col, must use col for EVERY expression)
+    carDF.select(col("CarName"), col("highwaympg"), col("citympg"), avgMPG.alias("avgmpg")).show(5)
+    // Another way (can trea each string as its own expression)
+    carDF.selectExpr("CarName", "highwaympg", "citympg", "((citympg + highwaympg) / 2) AS avgmpg").show(5)
+
+    // Find average price
+    carDF.select(avg("price")).show(5)
+    // Find average price statistics
+    carDF.select(avg("price")).describe().show(5)
+
+    // Spark SQL Functions
+    // Can use upper(), lower(), and camel() as a wrap for each column
+    // Can use trim(), ltrim(), and rtrim()
+    // Can use regex
+    carDF.select(col("CarName")).show(1)
+    // Create your regex
+    val extractStr = "(alfa|bmw|audi)"
+    // Use your regex (example)
+    // regexp_extract, regexp_replace
+    carDF.select(regexp_extract(col("CarName"), extractStr, 1).alias("extracted_data"), col("CarName")).show(10)
+    // contains(), replace()
+
+    // If the schema column does not match the schema, the column will be dropped****
+    // Ex. If any column in a given row is a null, entire row will be dropped here
+    println(carDF.na.drop("all").count())
+
+    // Replace with column name
+    println(carDF.na.fill("DUMMY VALUE", Seq("CarName", "carbody")).count())
+
+    // Can use more structured data by using StructType, ArrayType, and others
+
+    // explode
+    // exploding involves decomposing an array / struct to create a row for each element in the array / struct with the
+    // attributes of the original row for each new sub row
+    // Very bad for performance but can be a helpful tool
+
+    // Can read a value of a json as a txt file and then query the "key" as column name
+    // json_tuple will give you the value for the given key
+    // *There are also other json functions that you can use (explore self)
+    val countryDF = ss.range(1).selectExpr(""" '{"Singapore": {"Tier1": ["Yishun", "Bishan", "Woodlands"]}}' as jsonColumn""")
+    countryDF.printSchema()
+    countryDF.collect()
+    countryDF.select(json_tuple(col("jsonColumn"), "Singapore") as "extracted_data").show()
+
+    // Timestamps
+    // Look into date time function in spark functions
+
+    // UDF (User Defined Function
+    val tempCarDF = carDF.select(col("compressionratio") as "premium")
+    tempCarDF.show(5)
+    // Define a UDF that calculates the return on investment
+    // Sample UDF
+    def roi(x: Double): Double = x * 5 / 3
+    println(roi(10))
+    // Create the UDF
+    val roiUDF = udf(roi(_: Double): Double)
+    // User the UDF
+    tempCarDF.select(col("premium"), roiUDF(col("premium")).alias("roi")).show(5)
+
+    // Joins
+    // Inner Join - rows that include the common inner joined column
+    // Left outer Join - rows that include teh common inner joined column and left hand side columns
+    // Right outer Join - rows that include the common inner joined column and right hand side columns
+    // Full Outer Join (Outer Join) - left, right, and inner
+    // Left Semi Join - Only include left inner joins
+    // Right Semi Join - Only include right inner joins
+    // Natural Join - it will automatically join on all the fields having the same attributes (no specification of fields to join on)
+    // **Can do examples elsewhere
+
+    // More transformation actions
+
+    // Can use spark in-built functions or use external libraries in order to read in files
+
+    // To convert file formats, need to read in current format and then do an overwrite in a new format
 
   }
 
